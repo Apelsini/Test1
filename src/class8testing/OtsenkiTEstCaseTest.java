ba7486102afe15f89b7a8a85ff581b80fa5adeea
Apelsin <tre21jfk$>
2019-02-13 21:34:28 +0300

    test

--- src/class8testing/OtsenkiTEstCaseTest.java
+++ src/class8testing/OtsenkiTEstCaseTest.java
@@ -5,15 +5,17 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static class7collections.Otsenki.countAloneElements;
 import static class7collections.Otsenki.countUnique;
+import static org.junit.Assert.*;
 
-public class OtsenkiTEstCase {
+/**
+ * Created by user on 13.02.2019.
+ */
+public class OtsenkiTEstCaseTest {
     @Test
-    public void testCountUnique(){
-
+    public void testCountUnique() throws Exception {
         Assert.assertEquals(3, countUnique(Arrays.asList(8, 8, 9, 9, 9, 10)));
-        Assert.assertEquals(3, countUnique(Arrays.asList(1, 2, 3)));
+        Assert.assertEquals(2, countUnique(Arrays.asList(1, 2, 3)));
     }
-}
 
+}
